5ffa5766e585f97bf126d698bf6f8bee1526a01d
Zyrie Kyle Rallos <zykie1018@gmail.com>
2022-11-30 19:53:15 +0800

    configure postgres database

---
+++ app/tests.py
@@ -0,0 +1,26 @@
+"""
+    Sample tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTest(SimpleTestCase):
+    def test_add_numbers(self):
+        """
+            Add numbers together
+        """
+        
+        res = calc.add(5, 6)
+        
+        self.assertEqual(res, 11)
+        
+    def test_subtract_numbers(self):
+        """Test subtracting numbers
+        """
+        
+        res = calc.subtract(10, 15)
+        
+        self.assertEqual(res, 5)
